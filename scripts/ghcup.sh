1c2b34585464367e9e71765a97b75683f79312e7
Daniel Brathagen <dbrathagen@gmail.com>
2023-08-13 14:04:42 +0200

    Adding ghcup installation

---
+++ scripts/ghcup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+# Unattended installation of ghcup to xdg directories
+# Not modifying bashrc
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_GHC_VERSION=latest BOOTSTRAP_HASKELL_CABAL_VERSION=latest BOOTSTRAP_HASKELL_INSTALL_STACK=1 BOOTSTRAP_HASKELL_INSTALL_HLS=1 BOOTSTRAP_HASKELL_ADJUST_BASHRC='' GHCUP_USE_XDG_DIRS=1 sh
